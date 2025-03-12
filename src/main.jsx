f4d3c4332ce7e96553be46e4c3c195fc491bf8f4
NagarajanSanthosh <64208ns@gmail.com>
2025-03-13 01:57:48 +0530

    mistakes corrected - merge conflicts issues

--- src/main.jsx
+++ src/main.jsx
@@ -47,9 +47,12 @@ const router = createBrowserRouter([
     ]
   }
 ]);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <RouterProvider router={router} />
+
+
+
   </StrictMode>,
 )
